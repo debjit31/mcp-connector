bf881bcdbf2576c8ad5d680b9fee82de104f3da9
Debjit Chattopadhyay <debjitchatterjee31@gmail.com>
2025-09-22 22:46:26 +0530

    First Commit

---
+++ src/main/java/com/mcp_connector/McpConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.mcp_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcp_connector/McpConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
